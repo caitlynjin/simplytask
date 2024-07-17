a4560bc611ab99549e2d7566ac7672b974af0388
Caitlyn Jin <cj384@cornell.edu>
2024-07-17 02:13:39 -0700

    Implemented task functions and UI
    - Create, update, delete task functions
    - Get all tasks function
    - Responsive UI to create a task, complete a task, edit and delete tasks

--- app/TasksScreen.tsx
+++ app/TasksScreen.tsx
@@ -1,13 +1,14 @@
 import { auth } from '@/firebaseConfig';
-import { createTask } from '@/services/tasks';
+import { createTask, getAllTasks, updateTask, deleteTask } from '@/services/tasks';
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { } from 'firebase/firestore'
 import { CustomText } from "@/components/CustomText";
 import { CustomTextInput } from "@/components/CustomTextInput";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
@@ -15,81 +16,217 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Image,
-  TextInput
+  TextInput,
+  ScrollView
 } from "react-native";
+
+type Task = {
+  id: string;
+  completed: boolean;
+  description: string;
+  name: string;
+  inEdit: boolean;
+};
 
 export default function TasksScreen() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [addTaskToggled, setAddTaskToggled] = useState(false);
 
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   const fetchTasks = async () => {
+    if (auth.currentUser) {
+      const fetchedTasks = await getAllTasks(auth.currentUser.uid);
+      if (fetchedTasks) {
+        setTasks(fetchedTasks.map((task) => ({
+          ...task,
+          inEdit: false
+        })));
+      };
+    }
+  };
 
-  }
-
-  const cancelNewTask = () => {
+  const cancelNewTask = async () => {
     setAddTaskToggled(false);
-  }
+  };
 
   const addNewTask = async () => {
-    if (auth.currentUser) {
-      await createTask(auth.currentUser.uid, name, description);
+    if (auth.currentUser && name) {
+      await createTask(auth.currentUser.uid, name, description)
+        .then(fetchTasks);
     }
     setAddTaskToggled(false);
-  }
+    setName("");
+    setDescription("");
+  };
+
+  const checkTask = async (task: Task) => {
+    if (auth.currentUser) {
+      await updateTask(auth.currentUser.uid, task, !task.completed)
+        .then(fetchTasks);
+    }
+  };
+
+  const editTask = async (taskId: string) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === taskId ? {
+          ...prevTask,
+          inEdit: !prevTask.inEdit
+        } : prevTask
+      )
+    );
+  };
+
+  const doneEditTask = async (task: Task) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === task.id ? {
+          ...prevTask,
+          inEdit: !prevTask.inEdit
+        } : prevTask
+      )
+    );
+
+    if (auth.currentUser) {
+      await updateTask(auth.currentUser.uid, task, task.completed, description, name)
+        .then(fetchTasks);
+      setName("");
+      setDescription("");
+    }
+  };
+
+  const removeTask = async (task: Task) => {
+    if (auth.currentUser) {
+      await deleteTask(auth.currentUser.uid, task)
+        .then(fetchTasks);
+    }
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <CustomText type="title">My Tasks</CustomText>
-        </View>
-        <View style={styles.contentContainer}>
-          {/* Add task UI */}
-          {!addTaskToggled ?
-            <TouchableOpacity
-              onPress={() => setAddTaskToggled(true)}
-            >
-              <View style={styles.addTaskContainer}>
-                <CustomText type="mediumTitle">Add task</CustomText>
-              </View>
-            </TouchableOpacity>
-          : undefined}
-          {addTaskToggled ?
-            <View>
-              <View style={styles.addTaskContainer}>
-                <TouchableOpacity onPress={cancelNewTask}>
-                  <Image
-                    source={require('@/assets/images/X.png')}
-                    style={styles.xIcon}
-                  />
-                </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <CustomText type="title">My Tasks</CustomText>
+      </View>
 
-                <TextInput
-                  onChangeText={ (text) => setName(text) }
-                  placeholder='Enter task name here'
+      <ScrollView style={styles.contentContainer}>
+        {/* Add task UI */}
+        {!addTaskToggled ?
+          <TouchableOpacity
+            onPress={() => setAddTaskToggled(true)}
+          >
+            <View style={styles.addTaskContainer}>
+              <CustomText type="mediumTitle">Add task</CustomText>
+            </View>
+          </TouchableOpacity>
+        : undefined}
+
+        {addTaskToggled ?
+          <View>
+            <View style={styles.addTaskContainer}>
+              <TouchableOpacity onPress={cancelNewTask}>
+                <Image
+                  source={require('@/assets/images/X.png')}
+                  style={styles.xIcon}
+                />
+              </TouchableOpacity>
+
+              <TextInput
+                onChangeText={ (text) => setName(text) }
+                placeholder='Enter task name here'
+                style={styles.nameTextInput}
+              />
+            </View>
+            <View style={styles.addTaskBodyContainer}>
+              <TextInput
+                onChangeText={ (text) => setDescription(text) }
+                placeholder='Enter description here'
+                style={styles.descriptionTextInput}
+              />
+
+              <TouchableOpacity onPress={addNewTask}>
+                <Image
+                  source={require('@/assets/images/check.png')}
+                  style={styles.checkIcon}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        : undefined}
+
+        {/* Tasks UI */}
+        {tasks.map((task) => (
+          <View key={task.id}>
+            <View key={task.id} style={styles.taskContainer}>
+              <View style={styles.taskLeftPart}>
+                {task.inEdit
+                  // In edit state
+                  ? <TouchableOpacity onPress={() => removeTask(task)}>
+                    <Image
+                      source={require('@/assets/images/trash.png')}
+                      style={styles.checkboxIcon}
+                    />
+                  </TouchableOpacity>
+                  // Normal state
+                  : <TouchableOpacity onPress={() => checkTask(task)}>
+                    <Image
+                      source={
+                        task.completed
+                        ? require('@/assets/images/checkbox-filled.png')
+                        : require('@/assets/images/checkbox.png')
+                      }
+                      style={styles.checkboxIcon}
+                    />
+                  </TouchableOpacity>}
+                {task.inEdit
+                ? <TextInput
+                  onFocus={() => setName(task.name)}
+                  onChangeText={(text) => setName(text)}
+                  placeholder={task.name}
                   style={styles.nameTextInput}
+                  value={name}
                 />
+                : <CustomText type="mediumTitle">{task.name}</CustomText>}
               </View>
-              <View style={styles.addTaskBodyContainer}>
-                <TextInput
-                  onChangeText={ (text) => setDescription(text) }
-                  placeholder='Enter description here'
-                  style={styles.descriptionTextInput}
-                />
+            </View>
 
-                <TouchableOpacity onPress={addNewTask}>
+            <View style={styles.addTaskBodyContainer}>
+              {task.inEdit
+                ? <TextInput
+                  onFocus={() => setDescription(task.description)}
+                  onChangeText={(text) => setDescription(text)}
+                  placeholder={task.description}
+                  style={styles.descriptionTextInput}
+                  value={description}
+                />
+                : <CustomText type="body" style={{ width: 260 }}>
+                  {task.description}
+                </CustomText>}
+
+              {task.inEdit
+                // Edit state
+                ? <TouchableOpacity onPress={() => {doneEditTask(task)}}>
                   <Image
                     source={require('@/assets/images/check.png')}
                     style={styles.checkIcon}
                   />
                 </TouchableOpacity>
-              </View>
+                // Normal state
+                : <TouchableOpacity onPress={() => {editTask(task.id)}}>
+                  <Image
+                    source={require('@/assets/images/edit.png')}
+                    style={styles.checkIcon}
+                  />
+                </TouchableOpacity>}
             </View>
-          : undefined}
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
 }
 
@@ -107,7 +244,9 @@ const styles = StyleSheet.create({
     paddingBottom: 12
   },
   contentContainer: {
-    margin: 32,
+    paddingHorizontal: 32,
+    marginTop: 32,
+    marginBottom: 96
   },
   addTaskContainer: {
     flexDirection: "row",
@@ -118,6 +257,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#F1F1F1",
   },
   addTaskBodyContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+    gap: 16,
+    borderRadius: 8,
+    backgroundColor: "#F1F1F1",
+  },
+  taskContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: "#D9D9D9",
+  },
+  taskLeftPart: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12
+  },
+  taskBodyContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     paddingVertical: 12.5,
@@ -155,5 +318,9 @@ const styles = StyleSheet.create({
   checkIcon: {
     width: 26,
     height: 26
+  },
+  checkboxIcon: {
+    width: 21,
+    height: 21
   }
 });
--- services/tasks.ts
+++ services/tasks.ts
@@ -1,10 +1,31 @@
 import {
-  setDoc,
   doc,
   collection,
-  addDoc
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc
 } from 'firebase/firestore';
 import { db } from '@/firebaseConfig';
+
+export class Task {
+  id: string;
+  completed: boolean;
+  description: string;
+  name: string;
+
+  constructor(
+    id: string,
+    completed: boolean,
+    description: string,
+    name: string
+  ) {
+    this.id = id;
+    this.completed = completed;
+    this.description = description;
+    this.name = name;
+  }
+}
 
 export const createTask = async (
   userId: string,
@@ -12,13 +33,65 @@ export const createTask = async (
   description: string
 ) => {
   try {
-    const userRef = doc(db, "users", userId)
+    const userRef = doc(db, "users", userId);
     const tasksRef = collection(userRef, "tasks");
     addDoc(tasksRef, {
+      completed: false,
+      description: description,
       name: name,
-      description: description
+    })
+    .then((taskRef) => {
+      updateDoc(taskRef, {
+        id: taskRef.id
+      });
     });
   } catch (error) {
     console.error("Error creating task", error);
   }
 };
+
+export const getAllTasks = async (userId: string) => {
+  try {
+    const userTasksRef = collection(db, "users", userId, "tasks");
+    const userTasksSnap = await getDocs(userTasksRef);
+    const tasks = userTasksSnap.docs.map((taskDoc) =>
+      new Task(
+        taskDoc.get("id"),
+        taskDoc.get("completed"),
+        taskDoc.get("description"),
+        taskDoc.get("name")
+      )
+    );
+    return tasks;
+  } catch (error) {
+    console.error("Error getting all tasks", error);
+  }
+};
+
+export const updateTask = async (
+  userId: string,
+  task: Task,
+  completed?: boolean,
+  description?: string,
+  name?: string
+) => {
+  try {
+    const taskRef = doc(db, "users", userId, "tasks", task.id);
+    updateDoc(taskRef, {
+      completed: completed !== undefined ? completed : task.completed,
+      description: description ? description : task.description,
+      name: name ? name : task.name,
+    });
+  } catch (error) {
+    console.error("Error updating task", error);
+  }
+};
+
+export const deleteTask = async (userId: string, task: Task) => {
+  try {
+    const taskRef = doc(db, "users", userId, "tasks", task.id);
+    deleteDoc(taskRef);
+  } catch (error) {
+    console.error("Error updating task", error);
+  }
+}
